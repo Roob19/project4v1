211e6c22a3a8575f15588202e520384cf8795f97
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-16 15:47:06 -0700

    adding route to server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import logger from 'morgan';
+import cors from 'cors';
+
+import { router as barRouter } from './routes/bar';
+
+import('./config/database');
+
 const express = require('express');
 const app = express();
 const logger = require('morgan');
@@ -9,12 +20,24 @@ require('./config/database');
 const userRouter = require('./routes/users');
 const cors = require('cors')
 
+app.use(
+    express.static(
+        path.join(path.dirname(fileURLToPath(import.meta.url)), 'build')
+    )
+)
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+app.use('/api/bar', barRouter);
+
+app.get('/*', function(req, res) {
+    res.sendFile(
+        path.join(path.dirname(fileURLToPath(import.meta.url)), 'build', 'index.html')
+    )
+})
 
 app.listen(port, ()=> {
     console.log(`Express is listening on port ${port}.`)
